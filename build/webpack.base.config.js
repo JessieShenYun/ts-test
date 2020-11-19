ad1faa2d06f97199bceb9fcc27ca7a3bb5643fe0
yun.shen <yun.shen@Ctrip.com>
2020-11-19 20:43:12 +0800

    feat: ts test

---
+++ build/webpack.base.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'index': './src/index.ts' // 入口文件
+    },
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/i,
+                use: [{
+                    loader: 'ts-loader'
+                }],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/tpl/index.html'
+      })
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let hello: string = "hello, Shen Yun!"
+
+let dom = document.querySelector('.app')
+
+if (dom) {
+  dom.innerHTML = hello
+}
