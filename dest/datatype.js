b35189ec8a04489441c41ebf2f8e713c2ee2a5a4
yun.shen <yun.shen@Ctrip.com>
2020-11-20 14:24:51 +0800

    feat: ts type

--- dest/datatype.js
+++ dest/datatype.js
@@ -10,5 +10,56 @@ exports.arr1 = [1, 2, 3];
 exports.arr2 = [1, 2, 3, '4'];
 // 元组
 exports.tuple = [3, 'abc'];
-exports.tuple.push(2);
-console.log(exports.tuple);
+// 函数
+var add = function (x, y) { return x + y; };
+var compute = function (x, y) { return x + y; };
+// 对象
+var obj = { x: 1, y: 2 };
+// symbol
+var s1 = Symbol();
+var s2 = Symbol();
+console.log(s1 === s2); // false
+// undefined , null
+var un = undefined;
+var nu = null;
+// viod
+var noReturn = function () { };
+// any
+var x;
+// never
+var error = function () {
+    throw new Error('error');
+};
+var endless = function () {
+    while (true) { }
+};
+// 枚举类型
+var Role;
+(function (Role) {
+    Role[Role["Reporter"] = 0] = "Reporter";
+    Role[Role["Developer"] = 1] = "Developer";
+    Role[Role["Owner"] = 2] = "Owner";
+    Role[Role["Maintainer"] = 2] = "Maintainer";
+    Role[Role["Guest"] = 3] = "Guest";
+})(Role || (Role = {}));
+console.log(Role[2]);
+console.log(Role.Developer);
+// 字符串枚举
+var Message;
+(function (Message) {
+    Message["Success"] = "\u606D\u559C\u4F60\uFF0C\u6210\u529F\u4E86";
+    Message["Fail"] = "\u62B1\u6B49\uFF0C\u5931\u8D25\u4E86";
+})(Message || (Message = {}));
+// 异构枚举
+var Answer;
+(function (Answer) {
+    Answer[Answer["N"] = 0] = "N";
+    Answer["Y"] = "yes";
+})(Answer || (Answer = {}));
+// 枚举成员
+var a;
+(function (a) {
+    a[a["x"] = 'abc'.length] = "x";
+    a[a["y"] = 1] = "y";
+})(a || (a = {}));
+var month = [0 /* Jan */, 1 /* Feb */, 2 /* Mar */];
--- src/datatype.ts
+++ src/datatype.ts
@@ -9,6 +9,78 @@ export let arr2: Array<number | string> = [1, 2, 3, '4']
 
 // 元组
 export let tuple: [number, string] = [3, 'abc']
-tuple.push(2)
 
-console.log(tuple)
+// 函数
+let add = (x: number,y: number) => x + y
+let compute: (x: number, y: number) => number = (x, y) => x + y
+
+// 对象
+let obj: { x: number, y: number} = {x: 1, y: 2}
+
+// symbol
+let s1 : symbol = Symbol()
+let s2 = Symbol()
+console.log(s1 === s2)   // false
+
+// undefined , null
+let un: undefined = undefined
+let nu: null = null
+
+// viod
+let noReturn = () => {}
+
+// any
+let x: any
+
+// never
+let error = () => {
+  throw new Error('error')
+}
+let endless = () => {
+  while(true) {}
+}
+
+// 枚举类型
+enum Role {
+  Reporter = 0,
+  Developer = 1,
+  Owner = 2,
+  Maintainer = 2,
+  Guest = 3
+}
+console.log(Role[2])
+console.log(Role.Developer)
+
+// 字符串枚举
+enum Message {
+  Success = '恭喜你，成功了',
+  Fail = '抱歉，失败了'
+}
+
+// 异构枚举
+enum Answer {
+  N,
+  Y = 'yes'
+}
+
+// 枚举成员
+enum a {
+  x = 'abc'.length,
+  y = 1
+}
+
+// 常量枚举
+const enum Month {
+  Jan,
+  Feb,
+  Mar
+}
+
+let month = [Month.Jan, Month.Feb, Month.Mar]
+
+// 外部枚举
+declare enum Enum {
+  A = 1,
+  B,
+  C = 2
+}
